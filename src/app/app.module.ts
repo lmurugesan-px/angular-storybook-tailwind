6e074e38e0ef55b9e7362e5585917e05cf632606
lmurugesan <lmurugesan@purestorage.com>
2021-10-13 16:30:24 +0530

    initial setup for angular storybook and tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import ButtonComponent from 'src/stories/button.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
